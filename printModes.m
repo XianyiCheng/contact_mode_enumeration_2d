0aec69c4e0b9217ef1e3fdc423f5649e0d440086
Yifan Hou <yifanh@cmu.edu>
2020-01-01 09:29:18 -0500

    printModes return text string

--- printModes.m
+++ printModes.m
@@ -6,14 +6,15 @@
 %
 % @return     { description_of_the_return_value }
 %
-function printModes(contact_modes)
-
+function texts = printModes(contact_modes)
+texts = [];
 for i = 1:size(contact_modes,2)
     m = contact_modes(:,i);
     c = repmat('s',numel(m),1);
     c(m==1) = 'f';
     c(m==2) = 'r';
     c(m==3) = 'l';
-    fprintf(c);
-    fprintf('\n');
+    new_line = sprintf('%s\n',c);
+    texts = [texts new_line];
 end
+disp(texts);
